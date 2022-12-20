0a37eaaa8542e372a08b0bc3c0e6000d1b45aa5f
squalus <squalus@squalus.net>
2022-12-20 10:50:52 -0800

    add a wrapper for the nix binary

--- freshen.nix
+++ freshen.nix
@@ -1,6 +1,10 @@
-{ buildGoModule }:
+{ buildGoModule, makeWrapper, nix }:
 buildGoModule {
   name = "freshen";
   src = ./.;
   vendorHash = null;
+  nativeBuildInputs = [ makeWrapper ];
+  postInstall = ''
+    wrapProgram $out/bin/freshen --prefix PATH : ${nix}/bin
+  '';
 }
