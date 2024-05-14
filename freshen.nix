ab49911ab63f98b85a5c5f1e072d9eb0f021a76f
squalus <squalus@squalus.net>
2024-05-14 16:35:19 -0700

    unvendor deps

--- freshen.nix
+++ freshen.nix
@@ -1,8 +1,8 @@
-{ buildGoModule, makeWrapper, nix }:
+{ lib, buildGoModule, makeWrapper, nix }:
 buildGoModule {
   name = "freshen";
   src = ./.;
-  vendorHash = null;
+  vendorHash = "sha256-GToAKOOOGyHLTa3j1uPeC+7z2l6kI2XUP55CDlBdPZ0=";
   nativeBuildInputs = [ makeWrapper ];
   postInstall = ''
     wrapProgram $out/bin/freshen --prefix PATH : ${nix}/bin
