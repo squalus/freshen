87a7e0bc3f7168c6b8357b51f33af802730c6ddd
squalus <squalus@squalus.net>
2025-03-21 08:27:39 -0700

    update deps

--- freshen.nix
+++ freshen.nix
@@ -2,7 +2,7 @@
 buildGoModule {
   name = "freshen";
   src = ./.;
-  vendorHash = "sha256-OQERWnoR1pC+yftrt2fPOedOlwvoJUMAFxksLDv5Lvk=";
+  vendorHash = "sha256-WLgSNAo7TOM/cBNFmybmChxAEssJOGjEr2mZkSoZiYQ=";
   nativeBuildInputs = [ makeWrapper ];
   postInstall = ''
     wrapProgram $out/bin/freshen --prefix PATH : ${nix}/bin
